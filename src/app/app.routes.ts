2c5baca0cdf0e57177e25be7d780c136091ebeb9
Arturs Jansons <a.jansons@gmail.com>
2024-09-03 20:12:56 +0300

    feat: update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { BookShelfComponent } from './components/book-shelf/book-shelf.component';
+import { BookFormComponent } from './components/book-form/book-form.component';
+import { BookProfileComponent } from './components/book-profile/book-profile.component';
+
+export const routes: Routes = [
+  { path: '',
+    component: BookShelfComponent
+  },
+  { path: 'components/book-shelf',
+    component: BookShelfComponent
+  },
+  { path: 'components/book-form',
+    component: BookFormComponent
+  },
+  { path: 'components/book-profile',
+    component: BookProfileComponent
+  },
+  {
+    path: 'book-profile',
+    loadChildren: () =>
+      import('./components/book-profile/book-profile.module').then(
+        (m) => m.BookProfileModule
+      ),
+  },
+];

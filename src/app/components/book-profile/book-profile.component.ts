a742c4f3e1915fa40edf42a31dbb3e66bdc73426
Arturs Jansons <a.jansons@gmail.com>
2024-09-05 16:24:16 +0300

    feat: add read-only page for book

--- src/app/components/book-profile/book-profile.component.ts
+++ src/app/components/book-profile/book-profile.component.ts
@@ -1,14 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { BookFormComponent } from '../book-form/book-form.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BookService } from '../../services/book.service';
+import { ActivatedRoute } from '@angular/router';
+import { Book } from '../../models/book';
+
+const emptyBookFormValue = {
+  title: '',
+  author: '',
+  year: '',
+  pages: ''
+}
 
 @Component({
   selector: 'app-book-profile',
   standalone: true,
-  imports: [BookFormComponent, FormsModule],
+  imports: [BookFormComponent, FormsModule, ReactiveFormsModule],
   templateUrl: './book-profile.component.html',
   styleUrl: './book-profile.component.css',
 })
 export class BookProfileComponent {
+  @Input() bookId?: string;
+  value?: Omit<Book, 'id'>;
+  constructor(private bookService: BookService, private route: ActivatedRoute){
+    this.bookService.loadBooks();
+  }
+  ngOnInit() {
+    if (this.bookId) {
+      this.loadBook(this.bookId);
+    }
+    this.route.paramMap.subscribe(params => {
+      const bookId = params.get('id');
+      if (bookId) this.loadBook(bookId);
+    });
+  }
+  loadBook(id: string) {
+    if (!id) {
+      this.value = emptyBookFormValue
+      return;
+    }
 
+    setTimeout(() => {
+      if (this.bookId === id) return;
+      this.bookId = id;
+    }, 0);
+
+    this.bookService.getBookById(id).subscribe((book) => {
+      if (book?.id) {
+        this.value = {
+          title: book.title,
+          author: book.author,
+          year: book.year,
+          pages: book.pages
+        };
+      }
+    });
+  }
 }
